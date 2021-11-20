de286fcdfab72416b07e9a921bd373bea9941e84
surajkanojia05 <85305283+surajkanojia05@users.noreply.github.com>
2021-11-20 19:35:27 +0530

    Update settings.py

--- matrimony/settings.py
+++ matrimony/settings.py
@@ -148,24 +148,3 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-'''
-[
-    {
-        "AllowedHeaders": [
-            "*"
-        ],
-        "AllowedMethods": [
-            "GET",
-            "PUT",
-            "POST",
-            "HEAD",
-            "DELETE"
-        ],
-        "AllowedOrigins": [
-            "*"
-        ],
-        "ExposeHeaders": [],
-        "MaxAgeSeconds": 3000
-    }
-]
-'''
